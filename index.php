d426f76b1ee73134ca0c7c6603119e5dd8b515a4
alunofg <alunofg@fgacad.edu.br>
2022-06-13 09:59:21 -0300

    Tela de cadastro atualizado

--- index.php
+++ index.php
@@ -2,36 +2,34 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include "./partials/cabecalho.php" ?>
-    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include "./partials/cabecalho.php" ?>
+    <title>Login</title>
 </head>
 
 <body class="blue blue">
-    <div class="container center-align">
-        <div class="row blue lighten-3">
-            <div class="col s12 m6 offset-m3">
-                <h1 class="white-text">Login</h1>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="email" type="email" class="validate white-text" required>
-                        <label class="white-text" for="email">Email</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input id="password" type="password" class="validate white-text" required>
-                        <label class="white-text" for="password">Password</label>
-                    </div>
-                </div>
-                <a class="waves-effect waves-light btn" href="galeria.php">Entrar</a>
-                <br><br>
-                <a href="cadastrar-login.php">Ainda não cadastrado ? Cadastre-se no K7</a><br><br>
-            </div>
-        </div>
-    </div>
+    <div class="container center-align">
+        <div class="row blue lighten-3">
+            <div class="col s12 m6 offset-m3">
+                <h1 class="white-text">Bem Vindo!</h1>
+                <p class="subtitulo">Selecione a opção para fazer seu login</p>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <a class="waves-effect waves-light btn" href="galeria.php">Cliente</a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="input-field col s12">
+                        <a class="waves-effect waves-light btn" href="galeria.php">Gestor</a>
+                    </div>
+                </div>
+                <br><br>
+                <a href="cadastrar-login.php">Ainda não cadastrado ? Cadastre-se no K7</a><br><br>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
